9923d8817f9e6fa95e974f882eb2ad2dda5a2071
Soulter <905617992@qq.com>
2023-12-17 11:52:41 +0800

    feat: list

--- frontend/src/views/ConfigPage.vue
+++ frontend/src/views/ConfigPage.vue
@@ -19,6 +19,32 @@ import axios from 'axios';
             <template v-else-if="item.val_type === 'string'">
               <v-text-field v-model="item.value" :label="item.name" :hint="item.description" style="margin-bottom: 8px;" variant="outlined"></v-text-field>
             </template>
+            <template v-else-if="item.val_type === 'int'">
+              <v-text-field v-model="item.value" :label="item.name" :hint="item.description" style="margin-bottom: 8px;" variant="outlined"></v-text-field>
+            </template>
+            <template v-else-if="item.val_type === 'list'">
+              <span>{{ item.name }}</span>
+              <v-combobox
+                  v-model="item.value"
+                  chips
+                  clearable
+                  label="请添加"
+                  multiple
+                  prepend-icon="mdi-tag-multiple-outline"
+              >
+                  <template v-slot:selection="{ attrs, item, select, selected }">
+                  <v-chip
+                      v-bind="attrs"
+                      :model-value="selected"
+                      closable
+                      @click="select"
+                      @click:close="remove(item)"
+                  >
+                      <strong>{{ item }}</strong>
+                  </v-chip>
+                  </template>
+              </v-combobox>
+            </template>
           </template>
           <template v-else-if="item.config_type === 'divider'">
             <v-divider style="margin-top: 8px; margin-bottom: 8px;"></v-divider>
